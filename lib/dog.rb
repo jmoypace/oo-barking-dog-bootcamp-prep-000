a62f3eb66a1e3cf9557a66d27a58c776422a471d
jmoypace <jmoypace@gmail.com>
2018-05-27 02:53:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 # Your code goes here!
 class Dog
   def initialize(name, bark)
-    @.name = .name
-    @.bark = bark 
+    @name = .name
+    @bark = bark 
   end
  
   def .name=(.name)
-    @.name = .name
+    @name = .name
   end
  
   def .name
-    @.name
+    @name
   end
   
   def bark=(bark)
